3ccf95f0edbb9208a414b6cc7abe9da47570881b
unknown <fattila1101@gmail.com>
2019-06-18 13:59:12 +0200

    I've changed empty.js

--- js/empty.js
+++ js/empty.js
@@ -2,11 +2,25 @@ $(function()
 {
 	$('input[type="submit"]').on('click',function(e)
 	{
-		if($("#name").val()=="" || $("#pwd").val()=="")
+		if($("#name").val()=="" && $("#pwd").val()=="")
 		{
 			$("#name").addClass("border border-danger");
 			$("#pwd").addClass("border border-danger");
 			$(".error").html("Minden mező kitöltése kötelező!");
+			e.preventDefault();
+		}
+		else if($("#name").val()=="" && $("#pwd").val()!="")
+		{
+			$("#name").addClass("border border-danger");
+			$("#pwd").removeClass("border border-danger");
+			$(".error").html("Név mező kitöltése kötelező!");
+			e.preventDefault();
+		}
+		else if($("#name").val()!="" && $("#pwd").val()=="")
+		{
+			$("#name").removeClass("border border-danger");
+			$("#pwd").addClass("border border-danger");
+			$(".error").html("Jelszó mező kitöltése kötelező!");
 			e.preventDefault();
 		}
 	});
